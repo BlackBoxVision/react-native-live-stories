0c935c82b8a024891bd9df708720f7d55c2e926c
Manuel Tuero <manuel.tuero@blackbox-vision.tech>
2020-10-28 00:45:46 -0300

    feat(storypreview item): add linear gradient border to avatar

--- src/components/StoryPreviewItem/index.tsx
+++ src/components/StoryPreviewItem/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { ReactText } from 'react';
 import { Avatar } from 'react-native-elements';
-import { ActivityIndicator, ViewStyle } from 'react-native';
+import { ActivityIndicator, ViewStyle, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import { styles } from './styles';
 
@@ -25,24 +26,58 @@ export type StoryPreviewItemProps = {
    * The size of the Avatar component
    */
   size?: 'small' | 'medium' | 'large' | 'xlarge' | number;
+  /**
+   * The colors list of the gradient border
+   */
+  gradientBorderColors: ReactText[];
+  /**
+    Coordinates that declare the position that the gradient starts
+   */
+  gradientBorderStart?: { x: number; y: number };
+  /**
+    Coordinates that declare the position that the gradient ends
+   */
+  gradientBorderEnd?: { x: number; y: number };
+  /**
+    Array of numbers that defining the location of each gradient color stop, mapping to the color with the same index in colors prop
+   */
+  gradientBorderLocations?: number[];
+  /**
+    Gradient container styles
+   */
+  gradientContainerStyles?: ViewStyle;
 };
 
 export const StoryPreviewItem: React.FC<StoryPreviewItemProps> = ({
-  size = 'medium',
+  size = 'large',
   onPress,
   preview: uri,
   containerStyle,
   placeholderStyle,
+  gradientBorderColors,
+  gradientBorderStart,
+  gradientBorderEnd,
+  gradientBorderLocations,
+  gradientContainerStyles,
 }) => (
-  <Avatar
-    rounded
-    size={size}
-    source={{ uri }}
-    onPress={() => onPress()}
-    placeholderStyle={placeholderStyle}
-    containerStyle={[styles.container, containerStyle]}
-    renderPlaceholderContent={<ActivityIndicator color="#FFFFFF" />}
-  />
+  <View style={[styles.container, containerStyle]}>
+    <LinearGradient
+      colors={gradientBorderColors}
+      start={gradientBorderStart}
+      end={gradientBorderEnd}
+      locations={gradientBorderLocations}
+      style={[styles.gradientContainer, gradientContainerStyles]}
+    >
+      <Avatar
+        rounded
+        size={size}
+        source={{ uri }}
+        onPress={() => onPress()}
+        placeholderStyle={placeholderStyle}
+        renderPlaceholderContent={<ActivityIndicator color="#FFF" />}
+      />
+    </LinearGradient>
+  </View>
 );
 
 StoryPreviewItem.displayName = 'StoryPreviewItem';
