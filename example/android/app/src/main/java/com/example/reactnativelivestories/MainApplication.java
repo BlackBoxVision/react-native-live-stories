c3f1b64ad50c973e24661dd36814b58b36f303ef
Manuel Tuero <manuel.tuero@blackbox-vision.tech>
2020-11-01 17:58:17 -0300

    fix(assets): add missing fonts in android. fix ios duplicated fonts

--- example/android/app/src/main/java/com/example/reactnativelivestories/MainApplication.java
+++ example/android/app/src/main/java/com/example/reactnativelivestories/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
